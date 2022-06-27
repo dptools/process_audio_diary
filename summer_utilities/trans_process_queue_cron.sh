0f7265bd7db5d2c2ab02c6efe4bbcf417126df4b
ennisthemennis <mennis@mit.edu>
2022-06-27 19:43:26 +0100

    finally fix bsub cron to work properly on ERISTwo

--- summer_utilities/trans_process_queue_cron.sh
+++ summer_utilities/trans_process_queue_cron.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 source /PHShome/me741/.bashrc
+PATH="$PATH":/usr/share/lsf/9.1/linux2.6-glibc2.3-x86_64
+export PATH
+export LSF_ENVDIR=/usr/share/lsf/conf
+export LSF_SERVERDIR=/usr/share/lsf/9.1/linux2.6-glibc2.3-x86_64/etc
 
-bsub < /PHShome/me741/process_audio_diary/extra_utilities/queue_scripts/trans_process_queue.lsf
+/usr/share/lsf/9.1/linux2.6-glibc2.3-x86_64/bin/bsub < /PHShome/me741/process_audio_diary/extra_utilities/queue_scripts/trans_process_queue.lsf
